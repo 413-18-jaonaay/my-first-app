7d54e2e552499c8168909d774d2adf01a7e0369d
413-18-jaonaay <54470@yupparaj.ac.th>
2026-08-20 09:35:44 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -128,4 +128,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2, ans3, ans4)
  
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางสาวเจ้านาย เนตรทิพย์ เลขที่ 18 ม.4/13")
