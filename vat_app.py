91046df82f90cfe8bd0236bb6fc02ea271631618
413-18-jaonaay <54470@yupparaj.ac.th>
2026-09-03 09:35:28 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,12 +1,14 @@
 import streamlit as st
- 
-st.title("My dijitan Profile")
- 
-st.header("แนะนำตัว")
-st.write("น.ส. เจ้านาย เนตรทิพย์")
-st.write("ชื่อเล่น เจ้านาย")
-st.write("เลขที่18 4/13")
- 
-st.header("เป้าหมายและสิ่งที่อยากทำในอนาคต")
-st.write("เป้าหมาย : สอบผ่านทุกวิชา")
-st.write("สิ่งที่อยากทำในอนาคต : อยากเป็นผู้จัดการกองทุนรวม")
+
+st.title("📱 แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
+
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+
+vat = price * 0.07
+net_price = price - vat
+
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+
+st.divider()
+st.write("นางสาวเจ้านาย เนตรทิพย์ เลขที่ 18  ม.4/13")
